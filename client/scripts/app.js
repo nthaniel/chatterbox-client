8f8e5f3b6dc90edd6cb1379d73eac426e00cd91a
nthaniel <nedwards@gmail.com>
2016-03-14 15:16:19 -0700

    Add send and fetch methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,36 @@
-// YOUR CODE HERE:
+var app = {};
 
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.init = function () {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: failed to POST');
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      console.error('chatterbox: failed to GET');
+    }
+  });
+};
