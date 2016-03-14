e00d5fd8940529fe99f0757114f713ddefb52f1b
Kevin Nguyen <account@kevinwin.com>
2016-03-14 12:34:01 -0700

    Add escapeHtml function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,18 @@
-// YOUR CODE HERE:
+// EscapeHtml, courtesy of Mustache.js
+// Replaces certain characters with their HTML entity
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function fromEntityMap(s) {
+    return entityMap[s];
+  });
+}
 
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
